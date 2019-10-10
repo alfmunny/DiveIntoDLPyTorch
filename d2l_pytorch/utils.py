131d30799369cad7c3280016a63e5600a3da2f59
alfmunny <alfmunny@gmail.com>
2019-10-10 18:29:50 +0200

    3.6 added

---
+++ d2l_pytorch/utils.py
@@ -0,0 +1,78 @@
+from IPython import display
+from matplotlib import pyplot as plt
+import torchvision
+import torchvision.transforms as transforms
+import sys
+import torch
+
+def get_fashion_mnist_labels(labels):
+  text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+      'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+  return [text_labels[int(i)] for i in labels]
+
+def show_fashion_mnist(images, labels):
+  display.set_matplotlib_formats('svg')
+  _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+  for f, img, lbl in zip(figs, images, labels):
+    f.imshow(img.view((28, 28)).numpy())
+    f.set_title(lbl)
+    f.axes.get_xaxis().set_visible(False)
+    f.axes.get_yaxis().set_visible(False)
+  plt.show()
+
+def load_data_fashion_mnist(batch_size, root='~/Datasets/FashionMNIST'):
+    """Download the fashion mnist dataset and then load into memory."""
+    transform = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+    if sys.platform.startswith('win'):
+        num_workers = 0
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_iter, test_iter
+
+def evaluate_accuracy(data_iter, net):
+  acc_sum, n = 0.0, 0
+  for X, y in data_iter:
+    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+    n += y.shape[0]
+  return acc_sum / n
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, batch_size,
+              params=None, lr=None, optimizer=None):
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
+        for X, y in train_iter:
+            y_hat = net(X)
+            l = loss(y_hat, y).sum()
+
+            # 梯度清零
+            if optimizer is not None:
+                optimizer.zero_grad()
+            elif params is not None and params[0].grad is not None:
+                for param in params:
+                    param.grad.data.zero_()
+
+            l.backward()
+            if optimizer is None:
+                sgd(params, lr, batch_size)
+            else:
+                optimizer.step()  # “softmax回归的简洁实现”一节将用到
+
+
+            train_l_sum += l.item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().item()
+            n += y.shape[0]
+
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc))
+
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param.data.sub_(lr*param.grad/batch_size)
+
